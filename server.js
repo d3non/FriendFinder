70356bf0d52e3511fd5fe47cf096cd0647d80bb9
d3non <peps.valle@gmail.com>
2019-06-03 15:30:12 -0500

    Change port server 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
